10116e26736ab7e81ca1933a951a52d8f1512798
xjy2061 <xiongyunyang@HIH-L-13191.local>
2024-01-22 23:12:28 +0800

    Update exam SearchTree solution.

--- src/main/java/exam/SearchTree.java
+++ src/main/java/exam/SearchTree.java
@@ -7,42 +7,33 @@ public class SearchTree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] input = scanner.nextLine().split(",");
-        Node tree =  input[0].equals("null") ? null : new Node(Integer.parseInt(input[0]));
+        Node root =  input[0].equals("null") ? null : new Node(Integer.parseInt(input[0]));
         LinkedList<Node> nodes = new LinkedList<>();
-        nodes.add(tree);
+        nodes.add(root);
         for (int i = 1, len = input.length; i < len; i += 2) {
             Node node = nodes.remove();
             Node left = input[i].equals("null") ? null : new Node(Integer.parseInt(input[i]));
             int next = i + i;
-            Node right = next >= len || input[next].equals("null") ? null : new Node(Integer.parseInt(input[next]));
-            node.left = left;
-            node.right = right;
-            if (left != null) {
-                nodes.add(left);
+            Node right = (next >= len || input[next].equals("null")) ? null : new Node(Integer.parseInt(input[next]));
+            if (node != null) {
+                node.left = left;
+                node.right = right;
             }
-            if (right != null) {
-                nodes.add(right);
-            }
+            nodes.add(left);
+            nodes.add(right);
         }
 
-        System.out.println(inorder(tree));
+        System.out.println(isBST(root, Long.MIN_VALUE, Long.MAX_VALUE));
     }
 
-    private static int prev;
-
-    private static boolean inorder(Node node) {
-        if (node == null || (node.left == null && node.right == null)) {
+    private static boolean isBST(Node root, long min, long max) {
+        if (root == null) {
             return true;
         }
-        if (!inorder(node.left)) {
+        if (root.value <= min || root.value >= max) {
             return false;
         }
-        if (prev >= node.value) {
-            return false;
-        } else {
-            prev = node.value;
-        }
-        return inorder(node.right);
+        return isBST(root.left, min, root.value) && isBST(root.right, root.value, max);
     }
 
     static class Node {
