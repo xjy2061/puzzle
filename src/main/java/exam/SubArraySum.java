e65bc21058213f82c0f933bae104a4993d4292e8
xjy2061 <xiongyunyang@HIH-L-13191.local>
2024-03-17 21:04:12 +0800

    Add exam SubArraySum solution.

---
+++ src/main/java/exam/SubArraySum.java
@@ -0,0 +1,37 @@
+package exam;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SubArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(",");
+        int[] nums = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+        int k = scanner.nextInt();
+        if (nums.length < 2) {
+            System.out.println(0);
+            return;
+        }
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sum %= k;
+            Integer pre = map.get(sum);
+            if (pre != null) {
+                if (i - pre > 1) {
+                    System.out.println(1);
+                    return;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+        System.out.println(0);
+    }
+}
