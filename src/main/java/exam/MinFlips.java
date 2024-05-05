946b12cf97a0af81edfa8f291907e4aea513efc4
xjy2061 <xiongyunyang@HIH-L-13191.local>
2024-05-05 20:12:10 +0800

    Add exam MinFlips solution.

---
+++ src/main/java/exam/MinFlips.java
@@ -0,0 +1,30 @@
+package exam;
+
+import java.util.Scanner;
+
+public class MinFlips {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(",");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        int count = 0;
+        while (a > 0 || b > 0 || c > 0) {
+            int bitA = a & 1;
+            int bitB = b & 1;
+            int bitC = c & 1;
+            if ((bitA | bitB) != bitC) {
+                if (bitC == 0) {
+                    count += bitA + bitB;
+                } else {
+                    count++;
+                }
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        System.out.println(count);
+    }
+}
