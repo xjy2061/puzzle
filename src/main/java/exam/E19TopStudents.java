2f0a7b4d57d8421d6b2027a664cf1beae56893b5
xjy2061 <xjy2061@gmail.com>
2024-07-29 02:23:00 +0800

    Update exam TopStudents solution

--- src/main/java/exam/E19TopStudents.java
+++ src/main/java/exam/E19TopStudents.java
@@ -6,42 +6,28 @@ public class E19TopStudents {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] input = scanner.nextLine().split("-");
-        String[] positiveArray = input[0].substring(1, input[0].length() - 1).split(",");
-        String[] positives = new String[positiveArray.length];
-        for (int i = 0; i < positives.length; i++) {
-            positives[i] = positiveArray[i].substring(1, positiveArray[i].length() - 1);
-        }
-        String[] negativeArray = input[1].substring(1, input[1].length() - 1).split(",");
-        String[] negatives = new String[negativeArray.length];
-        for (int i = 0; i < negatives.length; i++) {
-            negatives[i] = negativeArray[i].substring(1, negativeArray[i].length() - 1);
-        }
+        String positiveStr = input[0].substring(1, input[0].length() - 1);
+        String negativeStr = input[1].substring(1, input[1].length() - 1);
         String[] reportArray = input[2].substring(1, input[2].length() - 1).split(",");
-        String[] reports = new String[reportArray.length];
-        for (int i = 0; i < reports.length; i++) {
-            reports[i] = reportArray[i].substring(1, reportArray[i].length() - 1);
-        }
         String[] idArray = input[3].substring(1, input[3].length() - 1).split(",");
-        int[] ids = new int[idArray.length];
-        for (int i = 0; i < ids.length; i++) {
-            ids[i] = Integer.parseInt(idArray[i]);
-        }
         int k = Integer.parseInt(input[4]);
-        int[][] scores = new int[ids.length][2];
-        for (int i = 0; i < ids.length; i++) {
-            String report = reports[i];
+        HashMap<String, Integer> wordScores = new HashMap<>();
+        for (String s : positiveStr.split(",")) {
+            wordScores.put(s.substring(1, s.length() - 1), 3);
+        }
+        for (String s : negativeStr.split(",")) {
+            wordScores.put(s.substring(1, s.length() - 1), -1);
+        }
+        int[][] scores = new int[idArray.length][2];
+        for (int i = 0; i < idArray.length; i++) {
+            String report = reportArray[i];
+            report = report.substring(1, report.length() - 1);
             int score = 0;
-            for (String positive : positives) {
-                if (report.contains(positive)) {
-                    score += 3;
-                }
+            for (String s : report.split(" ")) {
+                Integer ws = wordScores.get(s);
+                score += ws != null ? ws : 0;
             }
-            for (String negative : negatives) {
-                if (report.contains(negative)) {
-                    score -= 1;
-                }
-            }
-            scores[i] = new int[]{ids[i], score};
+            scores[i] = new int[]{Integer.parseInt(idArray[i]), score};
         }
         Arrays.sort(scores, (s1, s2) -> {
             if (s1[1] == s2[1]) {
@@ -50,10 +36,14 @@ public class E19TopStudents {
                 return s2[1] - s1[1];
             }
         });
-        int[] topK = new int[k];
+        StringBuilder ret = new StringBuilder("[");
         for (int i = 0; i < k; i++) {
-            topK[i] = scores[i][0];
+            if (i > 0) {
+                ret.append(", ");
+            }
+            ret.append(scores[i][0]);
         }
-        System.out.println(Arrays.toString(topK));
+        ret.append("]");
+        System.out.println(ret);
     }
 }
