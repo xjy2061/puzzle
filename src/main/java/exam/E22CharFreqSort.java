bf6beec478fb07551c18bc9f9c0c3ed23a9b8fe3
xjy2061 <xjy2061@gmail.com>
2024-10-09 13:12:55 +0800

    Add exam CharFreqSort solution

---
+++ src/main/java/exam/E22CharFreqSort.java
@@ -0,0 +1,28 @@
+package exam;
+
+import java.util.*;
+
+public class E22CharFreqSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
+        list.sort((a, b) -> {
+            if (!a.getValue().equals(b.getValue())) {
+                return b.getValue() - a.getValue();
+            }
+            return a.getKey() - b.getKey();
+        });
+        StringBuilder ret = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : list) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                ret.append(entry.getKey());
+            }
+        }
+        System.out.println(ret);
+    }
+}
