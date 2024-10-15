9da3361de8223dcdfd6026a60206a07f7afa9f94
xjy2061 <xiongyunyang@HIH-L-13191.local>
2024-10-15 21:46:25 +0800

    Add exam LongestCommonSubsequence solution

---
+++ src/main/java/exam/M15LongestCommonSubsequence.java
@@ -0,0 +1,24 @@
+package exam;
+
+import java.util.Scanner;
+
+public class M15LongestCommonSubsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        System.out.println(dp[m][n]);
+    }
+}
